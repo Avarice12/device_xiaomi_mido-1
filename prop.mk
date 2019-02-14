076d492421fe0e0397ccb2876e6eef0a62c50c2c
Flex1911 <dedsa2002@gmail.com>
2019-02-14 10:50:17 +0100

    vince: Don't advertise CDS mode values in HAL1
    * Some camera apps are using cds-mode-values to determine CDS support.
    * We don't want this as CDS is broken with the new kernel driver.

--- prop.mk
+++ prop.mk
@@ -53,11 +53,17 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 camera.display.lmax=1280x720 \
 camera.display.umax=1920x1080 \
+vendor.camera.hal1.packagelist=com.skype.raider,com.google.android.talk,com.whatsapp \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
+persist.camera.HAL3.enabled=1 \
+persist.camera.eis.enable=1 \
 camera.lowpower.record.enable=1 \
 media.camera.ts.monotonic=1 \
 persist.camera.gyro.disable=0 \
 persist.camera.isp.clock.optmz=0 \
 persist.camera.stats.test=5 \
+persist.camera.CDS=off \
+persist.camera.video.CDS=off \
 persist.vendor.qti.telephony.vt_cam_interface=1 \
 vidc.enc.dcvs.extra-buff-count=2
 
