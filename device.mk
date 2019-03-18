153e8105f8f2fca22c71de47899a886760487fc5
ZeeLog <Danjel.Zammit@gmail.com>
2019-03-18 13:08:34 +0100

    mido: wifi: remove libcld80211 package
    Change-Id: I9bf385d44809a4afbcd55b137ff2f550ddf335bc

--- device.mk
+++ device.mk
@@ -378,7 +378,6 @@ PRODUCT_COPY_FILES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    libcld80211 \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client \
