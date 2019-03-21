4f0a3e91d30cbc5ba5ee420384b9e35f66d9d8f8
Hung Phan <phandinhhungvp2001@gmail.com>
2019-03-21 18:02:52 +0700

    Go Official

--- havoc_mido.mk
+++ havoc_mido.mk
@@ -39,7 +39,7 @@ TARGET_VENDOR := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
+export HAVOC_BUILD_TYPE := Official
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
 
