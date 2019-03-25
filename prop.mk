a85b1c8165e86fea7d21194e390ff9797f149bde
Hung Phan <phandinhhungvp2001@gmail.com>
2019-03-25 23:02:31 +0000

    Revert "mido: kang QCOM Framework Boost from Sakura(Redmi 6 Pro) part2"
    This reverts commit def6b490de1e321cfdb02bd4d321266a8fdcb03e.

--- prop.mk
+++ prop.mk
@@ -165,15 +165,6 @@ persist.rild.nitz_short_ons_1="" \
 persist.rild.nitz_short_ons_2="" \
 persist.rild.nitz_short_ons_3=""
 
-#QTI Performance
-PRODUCT_PROPERTY_OVERRIDES += \
-vendor.enable_prefetch=1 \
-vendor.iop.enable_uxe=1 \
-vendor.iop.enable_prefetch_ofr=1 \
-vendor.perf.iop_v3.enable=1 \
-ro.vendor.at_library=libqti-at.so \
-persist.vendor.qti.games.gt.prof=1
-
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
 DEVICE_PROVISIONED=1 \
