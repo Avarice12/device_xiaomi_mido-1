3d3c8324eae2be23e4008c6d203009ede94b3f7b
Hung Phan <phandinhhungvp2001@gmail.com>
2019-03-29 04:51:51 +0000

    Revert "mido: wifi: remove libcld80211 package"
    This reverts commit 91601e540a69832ff721b7d783c3eb491874e53e.

--- device.mk
+++ device.mk
@@ -373,6 +373,7 @@ PRODUCT_COPY_FILES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    libcld80211 \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwpa_client \
