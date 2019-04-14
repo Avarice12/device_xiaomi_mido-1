SyberiaProject-Devices/platform_device_xiaomi_mido

mido: uncommonize tree
* Why use a common vendor for uncommon device tree...

setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2017-2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,51 +18,35 @@
 
 set -e
 
-INITIAL_COPYRIGHT_YEAR=2017
-
 # Required!
 export DEVICE=mido
-export DEVICE_COMMON=msm8953-common
 export VENDOR=xiaomi
-
 export DEVICE_BRINGUP_YEAR=2017
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-SYBERIA_ROOT="$MY_DIR"/../../..
+SYBERIA_ROOT="${MY_DIR}/../../.."
 
-HELPER="$SYBERIA_ROOT"/vendor/syberia/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="$SYBERIA_ROOT/vendor/syberia/build/tools/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
+
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
 
 # Initialize the helper
-setup_vendor "$DEVICE_COMMON" "$VENDOR" "$SYBERIA_ROOT" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${SYBERIA_ROOT}" false
 
 # Copyright headers and guards
-write_headers "mido tissot"
+write_headers
 
-# The standard common blobs
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
-# We are done!
+# Finish
 write_footers
 
-if [ -s "$MY_DIR"/../$DEVICE/proprietary-files.txt ]; then
-    # Reinitialize the helper for device
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "$DEVICE" "$VENDOR" "$SYBERIA_ROOT" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "$MY_DIR"/../$DEVICE/proprietary-files.txt true
-
-    # We are done!
-    write_footers
-fi

Avarice12/device_xiaomi_mido-1

setup-makefiles.sh
@@ -20,48 +20,35 @@ set -e
 
 # Required!
 export DEVICE=mido
-export DEVICE_COMMON=msm8953-common
 export VENDOR=xiaomi
-
 export DEVICE_BRINGUP_YEAR=2017
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
+
 HAVOC_ROOT="${MY_DIR}/../../.."
 
-HELPER="$HAVOC_ROOT/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="$HAVOC_ROOT/vendor/havoc/build/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
 
+INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
+
 # Initialize the helper
-setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${HAVOC_ROOT}" true
+setup_vendor "${DEVICE}" "${VENDOR}" "${DU_ROOT}" false
 
 # Copyright headers and guards
-write_headers "mido"
+write_headers
 
-# The standard common blobs
-write_makefiles "${MY_DIR}/proprietary-files-qc.txt" true
+# The standard device blobs
+write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
 
 # Finish
 write_footers
 
-if [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
-    # Reinitialize the helper for device
 
-    INITIAL_COPYRIGHT_YEAR="$DEVICE_BRINGUP_YEAR"
-    setup_vendor "${DEVICE}" "${VENDOR}" "${HAVOC_ROOT}" false
-
-    # Copyright headers and guards
-    write_headers
-
-    # The standard device blobs
-    write_makefiles "${MY_DIR}/../${DEVICE}/proprietary-files.txt" true
-
-    # Finish
-    write_footers
-fi