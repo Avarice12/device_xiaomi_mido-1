f488c676e0c738aebc6b38d20a6b396522bad550
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-12-28 06:30:01 +0700

    mido: Enable audio offload to fix audio flags issue
    AudioPolicyManager will always use to DEEP_BUFFER flags. When system request to change to another audio flags
    then audio policy services will failed and throw back to DEEP_BUFFER by default because AudioPolicyManager
    set it priority to DEEP_BUFFER flags when streaming music or media.
    
    Change prop files from "audio.offload.enable=false" to true can make device use other audio flags beside DEEP_BUFFER
    when streaming media or music exclusively using HPH path.
    
    This issue was come on oreo, and still happen on pie and on this android 10
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- prop.mk
+++ prop.mk
@@ -4,8 +4,8 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
 af.fast_track_multiplier=2 \
-audio.offload.disable=true \
 audio.offload.min.duration.secs=30 \
+audio.offload.disable=false \
 audio.offload.video=true \
 persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
