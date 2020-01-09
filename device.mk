1036e9b12b2651cd4338380524eb6fbd4066d621
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-09 23:00:41 +0700

    mido: Build sqlite3
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- device.mk
+++ device.mk
@@ -341,6 +341,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# SQlite
+PRODUCT_PACKAGES += \
+    sqlite3
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
