a1cf7f87a7d9cbcc6d5ed17cc78282b8cf2f9789
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 16:30:50 +0800

    mido: Build vendor variant libqti_vndfwk_detect

--- device.mk
+++ device.mk
@@ -307,7 +307,8 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
-    libqti_vndfwk_detect
+    libqti_vndfwk_detect \
+    libqti_vndfwk_detect.vendor
 
 # Ramdisk
 PRODUCT_PACKAGES += \
