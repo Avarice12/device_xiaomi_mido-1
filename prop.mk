dc1561c155f79f60816982442f5681545ceaf28e
KenHV <kenhv@null.net>
2020-03-30 19:40:55 +0800

    mido: prop: Improve touch detection
    Credits to @TwistedPrime
    Signed-off-by: KenHV <kenhv@null.net>
    Signed-off-by: blacksuan19 <i@blacksuan19.me>
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- prop.mk
+++ prop.mk
@@ -269,3 +269,23 @@ wifi.interface=wlan0
 # Wifi-Display
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.sys.wfd.virtual=0
+
+# Improve touch detection
+PRODUCT_PROPERTY_OVERRIDES += \
+touch.pressure.scale=0.001 \
+persist.sys.ui.hw=1 \
+view.scroll_friction=10 \
+touch.size.calibration=diameter \
+touch.size.scale=1 \
+touch.size.bias=0 \
+touch.size.isSummed=0 \
+touch.pressure.scale=0.001 \
+touch.orientation.calibration=none \
+touch.distance.calibration=none \
+touch.distance.scale=0 \
+touch.coverage.calibration=box \
+touch.gestureMode=spots \
+MultitouchSettleInterval=1ms \
+MultitouchMinDistance=1px \
+TapInterval=1ms \
+TapSlop=1px
