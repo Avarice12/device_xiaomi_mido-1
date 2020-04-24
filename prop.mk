9c30e9cb605e141cd12ed101f58c08d7d90e6b93
Rafiester <rafi.haqqi@gmail.com>
2020-04-24 21:33:48 +0530

    mido: prop: Include Dalvik VM
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- prop.mk
+++ prop.mk
@@ -62,6 +62,18 @@ persist.vendor.cne.feature=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.coresight.config=stm-events
 
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.heapgrowthlimit=256m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapmaxfree=8m \
+ro.sys.fw.dex2oat_thread_count=8
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
