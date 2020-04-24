8873606babdbc0db7e8b75e02ae27f62e74aa3b3
Rafiester <rafi.haqqi@gmail.com>
2020-04-24 23:41:33 +0530

    mido: Add more surfaceflinger to prop
    Also fix Video recording for real
    Commit Reference :
    - https://github.com/CarbonROM/android_device_xiaomi_whyred/commit/bfbb43f0f48e87aa29ef77e3f14cabf48fb659e0
    - https://github.com/CarbonROM/android_device_xiaomi_whyred/commit/7319c4da113a9cef0dc1e3e95519f6cd1c45a4b5
    
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -77,6 +77,7 @@ debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
 debug.sf.recomputecrop=0 \
+debug.sf.enable_gl_backpressure=1 \
 debug.sf.disable_backpressure=1 \
 dev.pm.dyn_samplingrate=1 \
 persist.demo.hdmirotationlock=false \
@@ -193,8 +194,20 @@ vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so
 
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.early_phase_offset_ns=1500000 \
+debug.sf.early_app_phase_offset_ns=1500000 \
+debug.sf.early_gl_phase_offset_ns=3000000 \
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+
+PRODUCT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
