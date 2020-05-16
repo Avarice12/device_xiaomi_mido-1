b358a71e5228de4fe64e9fe58c836d177728f6e0
Vishalcj17 <vishalcj@aospa.co>
2020-05-16 10:03:36 +0200

    mido: build libvulkan
    Signed-off-by: Vishalcj17 <vishalcj@aospa.co>
    Change-Id: Ib3890df3da65def3e08b522ce887dfbe89a8045d

--- device.mk
+++ device.mk
@@ -158,6 +158,7 @@ PRODUCT_PACKAGES += \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libdisplayconfig \
+    libvulkan \
     liboverlay \
     libdisplayconfig.vendor \
     libqdMetaData \
