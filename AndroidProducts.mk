9558e376816a021d595bc873b6f2ae4bf148e028
Rafiester <rafi.haqqi1@gmail.com>
2020-08-21 22:37:12 +0700

    mido: Reloaded-ify
    Signed-off-by: Rafiester <rafi.haqqi1@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/reloaded_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    reloaded_mido-user \
+    reloaded_mido-userdebug \
+    reloaded_mido-eng
